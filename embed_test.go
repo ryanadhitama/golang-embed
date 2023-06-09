b0bf227984a5b6cdd72ee1aa4f0fd53bd13c86cf
ryanadhitama <ryanadhitama2@gmail.com>
2023-06-09 08:26:25 +0800

    embed file

--- embed_test.go
+++ embed_test.go
@@ -4,10 +4,23 @@ import (
 	"fmt"
 	"testing"
 	_ "embed"
+	"io/fs"
+	"io/ioutil"
 )
 //go:embed version.txt
 var version string
 
 func TestEmbed(t *testing.T) {
 	fmt.Println(version)
+}
+
+//go:embed image.png
+var image []byte
+
+func TestEmbedFile(t *testing.T) {
+	err := ioutil.WriteFile("image_new.png", image, fs.ModePerm)
+
+	if err != nil {
+		panic(err)
+	}
 }
